b1e5758eb1fadb712f89090f8311de621ec454c7
S. Clay Smith <s.clay.smith@gmail.com>
2015-07-10 15:34:09 -0700

    Separated Phrase into its own file. Cleaned up Readalong formatting and worked on iOS Safari compatibility. Adjusted phrase chunking technique so that it is mostly invisible to user.

--- src/Readalong.js
+++ src/Readalong.js
@@ -24,9 +24,12 @@
 
  */
 
+'use strict';
+
 var React = require('react');
 var PureRenderMixin = require('react/addons').addons.PureRenderMixin;
 var isValidElement = require('react/addons').isValidElement;
+var Phrase = require('./Phrase');
 
 /*************************
  Statics
@@ -41,13 +44,17 @@ var speakingChild = {};
  Constants
  *************************/
 
+var timeoutDelay = 500;
+
+var maxPhraseLength = 180;
+
 var characterRanges = {
-	latinLetters: "\\u0041-\\u005A\\u0061-\\u007A\\u00C0-\\u017F\\u0100-\\u01FF\\u0180-\\u027F"
+  latinLetters: '\\u0041-\\u005A\\u0061-\\u007A\\u00C0-\\u017F\\u0100-\\u01FF\\u0180-\\u027F'
 };
 
 var regExp = {
-	abbreviations: new RegExp("[^" + characterRanges.latinLetters + "](e\\.g\\.)|(i\\.e\\.)|(mr\\.)|(mrs\\.)|(ms\\.)|(dr\\.)|(prof\\.)|(esq\\.)|(sr\\.)|(jr\\.)[^" + characterRanges.latinLetters + "]", "ig"),
-	innerWordPeriod: new RegExp("[0-9" + characterRanges.latinLetters + "]\\.[0-9" + characterRanges.latinLetters + "]", "ig")
+  abbreviations: new RegExp('[^' + characterRanges.latinLetters + '](e\\.g\\.)|(i\\.e\\.)|(mr\\.)|(mrs\\.)|(ms\\.)|(dr\\.)|(prof\\.)|(esq\\.)|(sr\\.)|(jr\\.)[^' + characterRanges.latinLetters + ']', 'ig'),
+  innerWordPeriod: new RegExp('[0-9' + characterRanges.latinLetters + ']\\.[0-9' + characterRanges.latinLetters + ']', 'ig')
 };
 
 
@@ -55,29 +62,29 @@ var regExp = {
  Punctuation Escaping
  *************************/
 
-/* 
- Escape likely false-positives of sentence-final periods. Escaping is performed by wrapping a character's ASCII 
- equivalent in double curly brackets, which is then reversed (deencodcoded) after delimiting. 
+/*
+ Escape likely false-positives of sentence-final periods. Escaping is performed by wrapping a character's ASCII
+ equivalent in double curly brackets, which is then reversed (deencodcoded) after delimiting.
  */
 function encodePunctuation(text) {
-	return text
-		/* Escape the following Latin abbreviations and English titles: e.g., i.e., Mr., Mrs., Ms., Dr., Prof., Esq., Sr., and Jr. */
-			.replace(regExp.abbreviations, function (match) {
-				return match.replace(/\./g, "{{46}}");
-			})
-		/* Escape inner-word (non-space-delimited) periods. For example, the period inside "Blast.js". */
-			.replace(regExp.innerWordPeriod, function (match) {
-				return match.replace(/\./g, "{{46}}");
-			});
+  return text
+    /* Escape the following Latin abbreviations and English titles: e.g., i.e., Mr., Mrs., Ms., Dr., Prof., Esq., Sr., and Jr. */
+      .replace(regExp.abbreviations, function (match) {
+        return match.replace(/\./g, '{{46}}');
+      })
+    /* Escape inner-word (non-space-delimited) periods. For example, the period inside 'Blast.js'. */
+      .replace(regExp.innerWordPeriod, function (match) {
+        return match.replace(/\./g, '{{46}}');
+      });
 }
 
-/* 
- Used to decode both the output of encodePunctuation() and punctuation that has been manually escaped by users. 
+/*
+ Used to decode both the output of encodePunctuation() and punctuation that has been manually escaped by users.
  */
 function decodePunctuation(text) {
-	return text.replace(/{{(\d{1,3})}}/g, function (fullMatch, subMatch) {
-		return String.fromCharCode(subMatch);
-	});
+  return text.replace(/{{(\d{1,3})}}/g, function (fullMatch, subMatch) {
+    return String.fromCharCode(subMatch);
+  });
 }
 
 /*************************
@@ -85,33 +92,33 @@ function decodePunctuation(text) {
  *************************/
 
 function getDelimiterRegex(delimiter) {
-	switch (delimiter) {
-		case "word":
-			/* Matches strings in between space characters. */
-			/* Note: Matches will include any punctuation that's adjoined to the word, e.g. "Hey!" will be a full match. */
-			/* Note: Remember that, with Blast, every HTML element marks the start of a brand new string. Hence, "in<b>si</b>de" matches as three separate words. */
-			return /\s*(\S+)\s*/;
-			break;
+  switch (delimiter) {
+    case 'word':
+      /* Matches strings in between space characters. */
+      /* Note: Matches will include any punctuation that's adjoined to the word, e.g. 'Hey!' will be a full match. */
+      /* Note: Remember that, with Blast, every HTML element marks the start of a brand new string. Hence, 'in<b>si</b>de' matches as three separate words. */
+      return /\s*(\S+)\s*/;
 
-		case "sentence":
-			/* Matches phrases either ending in Latin alphabet punctuation or located at the end of the text. (Linebreaks are not considered punctuation.) */
-			/* Note: If you don't want punctuation to demarcate a sentence match, replace the punctuation character with {{ASCII_CODE_FOR_DESIRED_PUNCTUATION}}. ASCII codes: .={{46}}, ?={{63}}, !={{33}} */
-			return /(?=\S)((?=[.。]{2,})?[^!?]+?[.…!?。！？]+|(?=\s+$)|$(?=\s*[′’'”″“"「『』」)»]+)*)/;
-			/* RegExp explanation (Tip: Use Regex101.com to play around with this expression and see which strings it matches): 
-			 - Expanded view: /(?=\S) ( ([.]{2,})? [^!?]+? ([.…!?]+|(?=\s+$)|$) (\s*[′’'”″“")»]+)* )
-			 - (?=\S) --> Match must contain a non-space character.
-			 - ([.]{2,})? --> Match may begin with a group of periods.
-			 - [^!?]+? --> Grab everything that isn't an unequivocally-terminating punctuation character, but stop at the following condition...
-			 - ([.…!?]+|(?=\s+$)|$) --> Match the last occurrence of sentence-final punctuation or the end of the text (optionally with left-side trailing spaces).
-			 - (\s*[′’'”″“")»]+)* --> After the final punctuation, match any and all pairs of (optionally space-delimited) quotes and parentheses.
-			 */
-			break;
+    case 'sentence':
+      /* Matches phrases either ending in Latin alphabet punctuation or located at the end of the text. (Linebreaks are not considered punctuation.) */
+      /* Note: If you don't want punctuation to demarcate a sentence match, replace the punctuation character with {{ASCII_CODE_FOR_DESIRED_PUNCTUATION}}. ASCII codes: .={{46}}, ?={{63}}, !={{33}} */
+      return /(?=\S)((?=[.。]{2,})?[^!?]+?[.…!?。！？]+|(?=\s+$)|$(?=\s*[′’'”″“"「『』」)»]+)*)/;
+      /* RegExp explanation (Tip: Use Regex101.com to play around with this expression and see which strings it matches):
+       - Expanded view: /(?=\S) ( ([.]{2,})? [^!?]+? ([.…!?]+|(?=\s+$)|$) (\s*[′’'”″“")»]+)* )
+       - (?=\S) --> Match must contain a non-space character.
+       - ([.]{2,})? --> Match may begin with a group of periods.
+       - [^!?]+? --> Grab everything that isn't an unequivocally-terminating punctuation character, but stop at the following condition...
+       - ([.…!?]+|(?=\s+$)|$) --> Match the last occurrence of sentence-final punctuation or the end of the text (optionally with left-side trailing spaces).
+       - (\s*[′’'”″“")»]+)* --> After the final punctuation, match any and all pairs of (optionally space-delimited) quotes and parentheses.
+       */
 
-		case "element":
-			/* Matches text between HTML tags. */
-			/* Note: Wrapping always occurs inside of elements, i.e. <b><span class="blast">Bolded text here</span></b>. */
-			return /(?=\S)([\S\s]*\S)/;
-	}
+    case 'element':
+      /* Matches text between HTML tags. */
+      /* Note: Wrapping always occurs inside of elements, i.e. <b><span class="blast">Bolded text here</span></b>. */
+      return /(?=\S)([\S\s]*\S)/;
+    default:
+      break;
+  }
 }
 
 
@@ -120,69 +127,110 @@ function getDelimiterRegex(delimiter) {
  *************************/
 
 function getVoiceForName(name) {
-	var voices = window.speechSynthesis.getVoices();
-	return voices.filter(function (voice) {
-		return voice.name === name;
-	})[0] || voices[0];
+  var voices = window.speechSynthesis.getVoices();
+
+  var namedVoice = voices.filter(function (voice) {
+      return voice.name === name;
+    })[0];
+
+  if (typeof namedVoice === 'object') {
+    return namedVoice;
+  }
+}
+
+function getVoiceForLanguage(lang, preferLocal) {
+  if (typeof preferLocal !== 'boolean') {
+    preferLocal = true;
+  }
+
+  var voices = window.speechSynthesis.getVoices();
+
+  var languageVoices = voices.filter(function (voice) {
+    return voice.lang === lang;
+  });
+
+  if (preferLocal) {
+    var localVoices = languageVoices.filter(function (voice) {
+      return voice.localService === true;
+    });
+
+    if (typeof localVoices[0] === 'object') {
+      return localVoices[0];
+    }
+  }
+
+  if (typeof languageVoices[0] === 'object') {
+    return languageVoices[0];
+  }
+}
+
+function getVoice(name, lang) {
+  var namedVoice = getVoiceForName(name);
+  if (typeof namedVoice === 'object') {
+    console.log('Readalong: Selected named voice', namedVoice);
+    return namedVoice;
+  }
+
+  var languageVoice = getVoiceForLanguage(lang, false);
+  if (typeof languageVoice === 'object') {
+    console.log('Readalong: Selected language voice', languageVoice);
+    return languageVoice;
+  }
+
+  var voices = window.speechSynthesis.getVoices();
+  return voices[0];
 }
 
 function chunkString(str, length) {
-	return str.match(new RegExp('[\\s\\S]{1,' + length + '}(?:\\s|$)', 'g'));
+  return str.match(new RegExp('[\\s\\S]{1,' + length + '}(?:\\s|$)', 'g'));
 }
 
 
 /*************************
  Components
  *************************/
-    
-var Phrase = React.createClass({
+
+var Readalong = React.createClass({
+  displayName: 'Readalong',
+
   propTypes: {
-    onMouseOver: React.PropTypes.func.isRequired,
-    onMouseOut: React.PropTypes.func.isRequired
+    children: React.PropTypes.node.isRequired,
+    delimiter: React.PropTypes.oneOf(['word', 'sentence', 'element']).isRequired,
+    lang: React.PropTypes.string.isRequired,
+    voiceName: React.PropTypes.string
   },
 
   mixins: [PureRenderMixin],
 
   getInitialState: function() {
     return {
-      activeClass: ""
-    }
+      voice: getVoice(this.props.voiceName, this.props.lang),
+      delimiterRegex: getDelimiterRegex(this.props.delimiter)
+    };
   },
 
-  render: function () {
-    return (
-        <span className={'readalong-phrase ' + this.state.activeClass}
-              onMouseOver={this.props.onMouseOver}
-              onMouseOut={this.props.onMouseOut}
-              ariaHidden="true">
-          {this.props.children}
-        </span>
-    );
-  }
-});
-	
-var Readalong = React.createClass({
-	propTypes: {
-		lang: React.PropTypes.string.isRequired,
-		delimiter: React.PropTypes.oneOf(['word', 'sentence', 'element']).isRequired,
-		voiceName: React.PropTypes.string
-	},
+  componentDidMount: function() {
+    // Fire a speech synthesis event inside touchstart so that the component will
+    // run for iOS users
+    var el = this.refs.readalong.getDOMNode();
 
-  mixins: [PureRenderMixin],
+    el.addEventListener('touchstart', function enableWithTouch() {
+      el.removeEventListener('touchstart', enableWithTouch, false);
 
-	getInitialState: function() {
-		return {
-			voice: getVoiceForName(this.props.voiceName),
-			delimiterRegex: getDelimiterRegex(this.props.delimiter)
-		}
-	},
-	
-	componentWillReceiveProps: function (newProps) {
-		this.setState({
-			voice: getVoiceForName(newProps.voiceName),
-			delimiterRegex: getDelimiterRegex(newProps.delimiter)
-		});
-	},
+      var msg = new SpeechSynthesisUtterance();
+      msg.text = '';
+      msg.volume = 0;
+
+      window.speechSynthesis.speak(msg);
+    }, false);
+  },
+
+  componentWillReceiveProps: function(newProps) {
+    this.setState({
+      voice: getVoice(newProps.voiceName, newProps.lang),
+      delimiterRegex: getDelimiterRegex(newProps.delimiter)
+    });
+  },
 
   _phraseIndex: 0,
 
@@ -192,117 +240,149 @@ var Readalong = React.createClass({
 
     var child = this.refs[ref];
 
-    if (mouseOverChild === child) return;
+    if (mouseOverChild === child) { return; }
     mouseOverChild = child;
 
-    window.speechSynthesis.cancel();
-
-    this._speechWillBegin(child);
-
-    var msg = new SpeechSynthesisUtterance();
-		msg.text = child.getDOMNode().textContent;
-		msg.lang = this.props.lang;
-		msg.voice = this.state.voice;
-    
-    msg.onerror = function() { this._speechDidEnd(child); }.bind(this);
-		msg.onend = function() { this._speechDidEnd(child); }.bind(this);
-    
-		window.speechSynthesis.speak(msg);
-	},
+    this._speak(ref);
+  },
 
   _onMouseOut: function(ref) {
     if (window.speechSynthesis.speaking) {
       mouseOverChildTimeout[ref] = window.setTimeout(function() {
         mouseOverChild = null;
         delete mouseOverChildTimeout[ref];
-      }, 500);
+      }, timeoutDelay);
     } else {
       mouseOverChild = null;
     }
   },
-  
-  _speechWillBegin: function(child) {
+
+  _onTouch: function(ref, event) {
+    event.stopPropagation();
+
+    this._speak(ref);
+  },
+
+  _speak: function(ref) {
+    console.log("about to speak");
+    var child = this.refs[ref];
+
+    window.speechSynthesis.cancel();
+
+    this._speechWillBegin(ref);
+
+    var textContent = child.getDOMNode().textContent;
+    var phrases = chunkString(textContent, maxPhraseLength);
+
+    var utterances = phrases.map(function(phrase) {
+      var msg = new SpeechSynthesisUtterance();
+      msg.text = phrase;
+      msg.lang = this.props.lang;
+
+      if (typeof this.state.voice === 'object') {
+        msg.voice = this.state.voice;
+      }
+
+      msg.addEventListener('end', this._speechDidEnd.bind(this, ref));
+      msg.addEventListener('error', this._speechDidError.bind(this, ref));
+
+      return msg;
+    }, this);
+
+    utterances.forEach(function (utterance) {
+      window.speechSynthesis.speak(utterance);
+    });
+  },
+
+  _speechWillBegin: function(ref) {
+    var child = this.refs[ref];
+
     speakingChild = child;
 
     child.setState({
-      activeClass: "readalong-active"
+      activeClass: 'readalong-active'
     });
   },
-  
-  _speechDidEnd: function(child) {
+
+  _speechDidEnd: function(ref) {
+    var child = this.refs[ref];
+
     if (window.speechSynthesis.speaking) {
-      if (speakingChild === child) return;
+      if (speakingChild === child) {
+        return;
+      }
     } else {
       speakingChild = null;
     }
 
     child.setState({
-      activeClass: ""
+      activeClass: ''
     });
   },
 
-	_wrapChildren: function (children) {
-		return React.Children.map(children, this._wrapChild);
-	},
-	
-	_wrapChild: function (child) {
-		if (isValidElement(child)) {			
-			if (child.props.silent) {
-				return child;
-			}
-			
-			return React.cloneElement(child, {}, this._wrapChildren(child.props.children));
-		}
+  _speechDidError: function(ref, error) {
+    console.error(error);
 
-		var phrases = [];
+    this._speechDidEnd(ref);
+  },
 
-		if (this.props.delimiter === "sentence") {
-			child = encodePunctuation(child);
-		}
+  _wrapChildren: function (children) {
+    return React.Children.map(children, this._wrapChild);
+  },
 
-		var matches = child.split(this.state.delimiterRegex);
-		
-		for(var i = 0; i < matches.length; i++) {
-			var matchText = matches[i];
+  _wrapChild: function (child) {
+    if (isValidElement(child)) {
+      if (child.props.silent) {
+        return child;
+      }
 
-			if (this.props.delimiter === "sentence") {
-				matchText = decodePunctuation(matchText);
-			}
+      return React.cloneElement(child, {}, this._wrapChildren(child.props.children));
+    }
 
-			if (matchText.trim() === "") {
-				phrases.push(" ");
-			} else {
-				var matchPhrases = chunkString(matchText, 180);
-				
-				for (matchPhrase of matchPhrases) {
-          var ref = 'phrase' + this._phraseIndex;
-					phrases.push(
-						<Phrase ref={ref}
-                    onTouchMove={this._onMouseOver.bind(this, ref)}
-                    onTouchEnd={this._onMouseOut.bind(this, ref)}
-                    onTouchCancel={this._onMouseOut.bind(this, ref)}
-							      onMouseOver={this._onMouseOver.bind(this, ref)}
-							      onMouseOut={this._onMouseOut.bind(this, ref)}>
-							{matchPhrase}
-						</Phrase>
-					);
-					this._phraseIndex++;
-				}	
-			}
-		}
-			
-		return phrases;
-	},
+    var phrases = [];
 
-	render: function() {
+    if (this.props.delimiter === 'sentence') {
+      child = encodePunctuation(child);
+    }
+
+    var matches = child.split(this.state.delimiterRegex);
+
+    for (var i = 0; i < matches.length; i++) {
+      var matchText = matches[i];
+
+      if (this.props.delimiter === 'sentence') {
+        matchText = decodePunctuation(matchText);
+      }
+
+      if (matchText.trim() === '') {
+        phrases.push(' ');
+        continue;
+      }
+
+      var ref = 'phrase' + this._phraseIndex;
+      phrases.push(
+          <Phrase onMouseOut={this._onMouseOut.bind(this, ref)}
+                  onMouseOver={this._onMouseOver.bind(this, ref)}
+                  onTouchStart={this._onTouch.bind(this, ref)}
+                  ref={ref}>
+            {matchText}
+          </Phrase>
+      );
+      this._phraseIndex++;
+    }
+
+    return phrases;
+  },
+
+  render: function() {
     this._phraseIndex = 0;
 
-		return (
-        <div className="readalong">
+    return (
+        <div className='readalong' ref='readalong'>
           {this._wrapChildren(this.props.children)}
         </div>
     );
-	}
+  }
 
 });
 
