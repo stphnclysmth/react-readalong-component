f6c9fa9c8d388443fdc6c49603c23ebc5833f66f
S. Clay Smith <s.clay.smith@gmail.com>
2015-07-10 15:33:11 -0700

    Fixed up .dotfiles and gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var gulp = require('gulp-help')(require('gulp'));
 var initGulpTasks = require('react-component-gulp-tasks');
 var ghPages = require('gulp-gh-pages');
@@ -5,7 +7,7 @@ var ghPages = require('gulp-gh-pages');
 
 /**
  * Task configuration is loaded from config.js
- * 
+ *
  * Make any changes to the source or distribution files
  * and directory configuration there
  */
@@ -15,10 +17,10 @@ var config = require('./gulpconfig');
 
 /**
  * Tasks are added by the react-component-gulp-tasks package
- * 
+ *
  * See https://github.com/JedWatson/react-component-gulp-tasks
  * for documentation.
- * 
+ *
  * You can also add your own additional gulp tasks if you like.
  */
 
@@ -26,5 +28,5 @@ initGulpTasks(gulp, config);
 
 gulp.task('deploy', function() {
   return gulp.src('./example/dist/**/*')
-      .pipe(ghPages());
+    .pipe(ghPages());
 });
